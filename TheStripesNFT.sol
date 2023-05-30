bdaa2f4059e25f9b27932f992fe70c3ab53044be
5star-rep <111691547+5star-rep@users.noreply.github.com>
2023-05-30 13:48:01 +0100

    Update TheStripesNFT.sol

--- TheStripesNFT.sol
+++ TheStripesNFT.sol
@@ -5,14 +5,14 @@ pragma solidity ^0.8.7;
 import "contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "contracts/access/Ownable.sol";
 
-contract TheStripesNFT is ERC721Enumerable, Ownable {
+contract DreadFulz is ERC721Enumerable, Ownable {
     using Strings for uint256;
 
     string public baseURI;
     string public baseExtension = ".json";
-    uint256 public cost = 0.05 ether;
-    uint256 public presaleCost = 0.03 ether;
-    uint256 public maxSupply = 992;
+    uint256 public cost = 0.3 ether;
+    uint256 public presaleCost = 0.1 ether;
+    uint256 public maxSupply = 10000;
     uint256 public maxMintAmount = 20;
     bool public paused = false;
     mapping(address => bool) public whitelisted;
@@ -145,10 +145,10 @@ contract TheStripesNFT is ERC721Enumerable, Ownable {
         presaleWallets[_user] = false;
     }
 
-    function withdraw() public payable onlyOwner {
-        (bool success, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
-        require(success);
+    function withdraw() public onlyOwner {
+        require(payable(msg.sender).send(address(this).balance));
+           
+   
+     
     }
 }
